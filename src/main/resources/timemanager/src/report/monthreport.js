55dc560904c03d683314ffa5edfa09b5d1fdc3a8
darkusagi <darkusagi99@gmail.com>
2019-11-08 11:31:29 +0100

    Affichage en tableau - initialisation

--- src/main/resources/timemanager/src/report/monthreport.js
+++ src/main/resources/timemanager/src/report/monthreport.js
@@ -14,40 +14,72 @@ class ReportPresence extends Component {
                       depatureTime : new Date(),
                       hasMeal : false,
                       peoples: [],
-                      previousPresence: ''
+                      previousPresence: '',
+                      presences: [],
+                      peoples: []
                   }
 
         }
 
+        componentDidMount() {
+                    fetch('http://localhost:8080/presence/')
+                    .then(res => res.json())
+                    .then((data) => {
+                        this.setState({ presences: data })
+                    })
+                    .catch(console.log)
 
-        getDaysInMonth = (month, year) => (new Array(31)).fill('').map((v,i)=>new Date(year,month-1,i+1)).filter(v=>v.getMonth()===month-1)
+
+                    fetch('http://localhost:8080/people/')
+                    .then(res => res.json())
+                    .then((data) => {
+                        this.setState({ peoples: data })
+                    })
+                    .catch(console.log)
+
+        }
+
+
+        getDaysInMonth = () => (new Array(31)).fill('').map((v,i)=>new Date((new Date).getFullYear(),(new Date).getMonth(),i+1)).filter(v=>v.getMonth()===(new Date).getMonth())
 
         render() {
             return (
                 <div style={{marginTop: 10}}>
-                    <h3>Report Presence</h3>
-                    <form>
-                        <div className="form-group">
-                            <label>Name:  </label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Email: </label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label>Password: </label>
-                            <input type="text" className="form-control"/>
-                        </div>
-                         <div className="form-group">
-                            <input type="submit" value="Save" className="btn btn-primary"/>
-                        </div>
+                    <h3>Month Report</h3>
 
-                        {this.getDaysInMonth(2019, 10).map((dayInMonth) => (
-                                 "<h6>{dayInMonth.toLocaleDateString()}</h6>"
-                         ))}
+                        <table class="table">
+                        <thead>
+                             <tr>
 
-                    </form>
+                                 <th>Peoples</th>
+                                 {this.getDaysInMonth().map((dayInMonth) => (
+                                    <th>{dayInMonth.getDate()}</th>
+                                 ))}
+                             </tr>
+                        </thead>
+                        <tbody>
+
+                        {this.state.peoples.map((people) => (
+                             <tr>
+                                <td>{people.fullname}</td>
+                                {this.getDaysInMonth().map((dayInMonth) => (
+                                         <td>{this.state.presences
+                                            .filter((presence) => (people.id == presence.personId))
+                                            .filter((presence) => (dayInMonth.getFullYear() == new Date(presence.presenceDay).getFullYear()))
+                                            .filter((presence) => (dayInMonth.getMonth() == new Date(presence.presenceDay).getMonth()))
+                                            .filter((presence) => (dayInMonth.getDate() == new Date(presence.presenceDay).getDate()))
+                                            .map((presence) => (
+
+                                                new Date(presence.presenceDay).toISOString())
+                                            )}
+                                         </td>
+                                ))}
+                             </tr>
+                        ))}
+
+                        </tbody>
+                        </table>
+
                 </div>
             )
         }
